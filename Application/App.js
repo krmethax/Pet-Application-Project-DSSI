0fbb778d2508c46ff9f859589abf2fb370f0a573
krmethax <methasit6061@gmail.com>
2024-12-11 16:04:04 +0700

    11 dec 2024

--- Application/App.js
+++ Application/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'IBMPlexSansThai-Regular': require('./assets/fonts/IBMPlexSansThai-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
+    <View >
+      <SafeAreaView>
+        <Text style={{ fontFamily: 'IBMPlexSansThai-Regular' }}>สวัสดี จาก font IBM Plex Sans Thai</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
